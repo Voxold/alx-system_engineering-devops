92ec5cda5071e3c67a57fa8c97c57a570352f1c0
Voxold <bilalajaha@gmail.com>
2024-03-26 18:31:55 -0700

    The last

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,67 @@
+# Configuring server with Puppet!
+
+package { 'nginx':
+  ensure => 'installed',
+}
+
+exec { 'mk dir':
+    path    => '/usr/bin',
+    command => 'sudo mkdir -p /var/www/bilalajaha.tech/html',
+    returns => [0, 1],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+    ensure  => 'present',
+    content => "Hello World!\n",
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+}
+
+file { '/var/www/bilalajaha.tech/html/custom_404.html':
+    ensure  => 'present',
+    content => "Ceci n'est pas une page\n",
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+    ensure  => 'present',
+    content => "server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+
+        root /var/www/html;
+
+        index index.html index.htm index.nginx-debian.html;
+
+        server_name bilalajaha.tech;
+        add_header X-Served-By ${{hostname}};
+
+        location /hbnb_static/ {
+                alias /data/web_static/current/;
+        }
+
+        error_page 404 /custom_404.html;
+        location = /custom_404.html {
+                internal;
+                root /var/www/bilalajaha.tech/html;
+        }
+
+        location /redirect_me {
+                return 301 https://www.linkedin.com/in/voxold/;
+        }
+
+        location / {
+                root /var/www/bilalajaha.tech/html;
+                index index.html;
+                try_files ${{uri}} ${{uri}}/ =404;
+        }
+}
+"
+}
+
+service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+}
