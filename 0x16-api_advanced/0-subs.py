efd700341f028102f47afc8eacca12d21c018d03
Voxold <bilalajaha@gmail.com>
2024-05-07 16:40:07 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-'''This module contains functions for working with the Reddit API.
-'''
+""" requests number of subscribers from reddit api """
+import re
 import requests
 
 
-BASE_URL = 'https://www.reddit.com'
-'''Reddit's base API URL.
-'''
-
-
 def number_of_subscribers(subreddit):
-    '''Retrieves the number of subscribers in a given subreddit.
-    '''
-    api_headers = {
-        'Accept': 'application/json',
-        'User-Agent': ' '.join([
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'AppleWebKit/537.36 (KHTML, like Gecko)',
-            'Chrome/97.0.4692.71',
-            'Safari/537.36',
-            'Edg/97.0.1072.62'
-        ])
-    }
-    res = requests.get(
-        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-        headers=api_headers,
-        allow_redirects=False
-    )
-    if res.status_code == 200:
-        return res.json()['data']['subscribers']
-    return 0
+    """ returns the number of subscribers for a given subreddit"""
+
+    url = f"https://www.reddit.com/r/{subreddit}/about/"
+    res = requests.get(url)
+    html = res.text
+    pattern = r'subscribers="([^"]*)"'
+    subscribers = re.search(pattern, html)
+
+    if subscribers:
+        return int(subscribers[0][13:-1])
+    else:
+        return 0
