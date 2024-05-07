647b4da836aca6071f54382b628cbea02780f530
Voxold <bilalajaha@gmail.com>
2024-05-07 16:46:38 -0700

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """returns the top ten posts of a subreddit"""
 
     url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
-    res = requests.get(url, headers={'User-Agent': 'abeer'})
+    res = requests.get(url, headers={'User-Agent': 'Voxold'})
 
     if res.status_code != 200:
         print(None)
