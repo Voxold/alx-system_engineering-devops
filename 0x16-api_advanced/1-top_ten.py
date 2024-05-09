ba9f3dca5c271011a6e5c9093d8873c8ffda1a9f
Voxold <bilalajaha@gmail.com>
2024-05-09 07:24:04 -0700

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""request top ten posts of a subreddit"""
+"""Module to query the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def top_ten(subreddit):
-    """returns the top ten posts of a subreddit"""
-
-    url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
-    res = requests.get(url, headers={'User-Agent': 'Voxold'})
-
-    if res.status_code != 200:
+    """Returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        for i in range(10):
+            title = response.json()["data"]["children"][i]["data"]["title"]
+            print(title)
+    else:
         print(None)
-        return
-
-    top = res.json()
-    for post in top['data']['children']:
-        print(post['data']['title'])
